dacd3e1bfc81660cac9343c09a562fdcf3e3cbaa
iamschulz <hallo@iamschulz.de>
2024-03-18 20:33:24 +0100

    update service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,9 @@
-const ver = '1.1.1';
+const ver = "1.1.2";
 const cacheName = `js-synth-${ver}`;
-const filesToCache = [
-	'./',
-	'./index.html',
-	'./style.css',
-	'./script.js',
-	'./freqs.js',
-	'./keys.js',
-];
+const filesToCache = ["./", "./index.html", "./style.css", "./script.js", "./freqs.js", "./keys.js"];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function (e) {
 	e.waitUntil(
 		caches.open(cacheName).then(function (cache) {
 			return cache.addAll(filesToCache);
@@ -19,10 +12,12 @@ self.addEventListener('install', function (e) {
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
+self.addEventListener("fetch", function (e) {
 	e.respondWith(
-		caches.match(e.request).then(function (response) {
-			return response || fetch(e.request);
+		caches.open(cacheName).then(function (cache) {
+			return cache.match(e.request).then(function (response) {
+				return response || fetch(e.request);
+			});
 		})
 	);
 });
