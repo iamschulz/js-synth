de5ea18d76c69fca49b686224121a8decac8ce6f
Daniel Schulz <hallo@iamschulz.de>
2024-03-20 23:02:30 +0100

    parse midi data

--- midi.js
+++ midi.js
@@ -1,6 +1,6 @@
 export class MidiAdapter {
 	constructor() {
-		this.midi = null; // global MIDIAccess object
+		this.midi = null;
 		navigator
 			.requestMIDIAccess()
 			.then(this.onMIDISuccess.bind(this), this.onMIDIFailure.bind(this));
@@ -8,18 +8,20 @@ export class MidiAdapter {
 
 	onMIDISuccess(midiAccess) {
 		console.log("MIDI ready!");
-		this.midi = midiAccess; // store in the global (in real usage, would probably keep in an object instance)
-		this.startLoggingMIDIInput();
+		this.midi = midiAccess;
 	}
 
 	onMIDIFailure(msg) {
 		console.error(`Failed to get MIDI access - ${msg}`);
 	}
 
-	startLoggingMIDIInput() {
-		this.midi.inputs.forEach((entry) => {
-			entry.onmidimessage = this.onMIDIMessage;
-		});
+	parseMidiMessage(message) {
+		return {
+			command: message[0] >> 4,
+			channel: message[0] & 0xf,
+			note: message[1],
+			velocity: message[2] / 127,
+		};
 	}
 
 	onMIDIMessage(event) {
@@ -27,9 +29,29 @@ export class MidiAdapter {
 		for (const character of event.data) {
 			str += `0x${character.toString(16)} `;
 		}
+		str = str.trim();
 		if (str === "0xfe") {
 			return;
 		}
-		console.log(str);
+		const message = str.split(" ");
+		const data = this.parseMidiMessage(message);
+
+		if (data.channel === 0 && data.command === 9 && data.velocity > 0) {
+			this.pressNote(data);
+		}
+		if (
+			(data.channel === 0 && data.command === 8) ||
+			(data.command === 9 && data.velocity === 0)
+		) {
+			this.releaseNote(data);
+		}
+	}
+
+	pressNote(data) {
+		console.log("press note", data.note, data.velocity);
+	}
+
+	releaseNote(data) {
+		console.log("release note", data.note);
 	}
 }
