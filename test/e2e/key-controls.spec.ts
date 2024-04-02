44865ff7eba154be8b217d7f911c87aec9969394
iamschulz <hallo@iamschulz.de>
2024-04-02 09:55:52 +0200

    test key controls

--- test/e2e/key-controls.spec.ts
+++ test/e2e/key-controls.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 import { sleep } from "../helpers";
 
-test("virtual-keyboard", async ({ page, browserName }) => {
+test("key controls", async ({ page, browserName }) => {
 	if (browserName === "firefox") {
 		return; // todo: fix handles for firefox
 	}
@@ -15,17 +15,17 @@ test("virtual-keyboard", async ({ page, browserName }) => {
 	await page.goto("/");
 	await sleep(1000);
 
-	await page.getByRole("button", { name: "z" }).dispatchEvent("mousedown");
+	await page.keyboard.down("KeyQ");
 	await page.evaluate(() => console.log("test:", window.Synth.nodes));
-	await expect(logs[0]).toContain("f2: ");
+	await expect(logs[0]).toContain("c2: ");
 
-	await page.getByRole("button", { name: "u" }).dispatchEvent("mousedown");
+	await page.keyboard.down("KeyE");
 	await page.evaluate(() => console.log("test:", window.Synth.nodes));
-	await expect(logs[1]).toContain("f2: ");
-	await expect(logs[1]).toContain("fs2: ");
+	await expect(logs[1]).toContain("c2: ");
+	await expect(logs[1]).toContain("d2: ");
 
-	await page.getByRole("button", { name: "z" }).dispatchEvent("mouseup");
+	await page.keyboard.up("KeyQ");
 	await page.evaluate(() => console.log("test:", window.Synth.nodes));
-	await expect(logs[2]).toContain("fs2: ");
-	await expect(logs[2]).not.toContain("f2: ");
+	await expect(logs[2]).not.toContain("c2: ");
+	await expect(logs[2]).toContain("d2: ");
 });
