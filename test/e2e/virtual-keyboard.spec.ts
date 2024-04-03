c55bafe1825871b65774532971b9c0b13535bb69
iamschulz <hallo@iamschulz.de>
2024-04-03 22:52:16 +0200

    debug tests

--- test/e2e/virtual-keyboard.spec.ts
+++ test/e2e/virtual-keyboard.spec.ts
@@ -5,12 +5,12 @@ test("virtual-keyboard", async ({ page }) => {
 	await page.goto("/");
 	await sleep(500);
 
-	await page.getByRole("button", { name: "i" }).dispatchEvent("mousedown");
+	await page.locator('css=button[data-note="g2"]').dispatchEvent("mousedown");
 	await expect(await getNodes(page)).toStrictEqual(["g2"]);
 
-	await page.getByRole("button", { name: "u" }).dispatchEvent("mousedown");
+	await page.locator('css=button[data-note="fs2"]').dispatchEvent("mousedown");
 	await expect(await getNodes(page)).toStrictEqual(["g2", "fs2"]);
 
-	await page.getByRole("button", { name: "i" }).dispatchEvent("mouseup");
+	await page.locator('css=button[data-note="g2"]').dispatchEvent("mouseup");
 	await expect(await getNodes(page)).toStrictEqual(["fs2"]);
 });
