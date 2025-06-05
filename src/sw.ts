f165c74bfe9938b4f63f18ee69baba3815510ad0
iamschulz <hallo@iamschulz.de>
2025-06-05 12:28:05 +0200

    update version

--- src/sw.ts
+++ src/sw.ts
@@ -1,12 +1,6 @@
-const ver = "1.2.0";
+const ver = "1.1.7";
 const cacheName = `js-synth-${ver}`;
-const filesToCache = [
-	"./",
-	"./index.html",
-	"./style.css",
-	"./script.js",
-	"./press-start-2p-latin-400-normal.woff2",
-];
+const filesToCache = ["./", "./index.html", "./style.css", "./script.js", "./press-start-2p-latin-400-normal.woff2"];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", function (e) {
